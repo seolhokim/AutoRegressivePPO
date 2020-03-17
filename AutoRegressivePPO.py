fc3d09227f42c0ccb54c7c218010fbc38ed5f0e3
seolhokim <kilmya1@naver.com>
2020-03-17 21:16:52 +0900

    [Add] add Agent

--- AutoRegressivePPO.py
+++ AutoRegressivePPO.py
@@ -74,3 +74,84 @@ class AutoRegressivePPO(nn.Module):
 
         return [action_x_sample,action_y_sample],\
                 [action_x_log_prob,action_y_log_prob],value
+
+
+class Agent(nn.Module):
+    def __init__(self):
+        super(Agent,self).__init__()
+        self.model = AutoRegressivePPO()
+        
+        self.memory = []
+        self.optimizer = optim.Adam(self.parameters(),lr = learning_rate)
+        
+    def get_action(self,x,action_sequence = []):
+        action, prob,value = self.model(x,action_sequence)
+        return action,prob
+    
+    def get_value(self,x):
+        return self.model(x)[2]
+    
+    def put_data(self,data):
+        self.memory.append(data)
+        
+    def make_batch(self):
+        state_list, action_x_list, action_y_list,\
+        reward_list, next_state_list, action_x_prob_list,\
+        action_y_prob_list, done_list = [],[],[],[],[],[],[],[]
+        for data in self.memory:
+            state,action_x,action_y,reward,next_state,action_x_prob,action_y_prob,done = data
+            state_list.append(state)
+            action_x_list.append([action_x])
+            action_y_list.append([action_y])
+            reward_list.append([reward])
+            next_state_list.append(next_state)
+            action_x_prob_list.append([action_x_prob])
+            action_y_prob_list.append([action_y_prob])
+            done_mask = 0 if done else 1
+            done_list.append([done_mask])
+        self.memory = []
+        
+        s,action_x,action_y,r,next_s,action_x_prob,action_y_prob, done_mask =\
+                                        torch.tensor(state_list,dtype=torch.float),\
+                                        torch.tensor(action_x), torch.tensor(action_y),\
+                                        torch.tensor(reward_list),\
+                                        torch.tensor(next_state_list,dtype=torch.float),\
+                                        torch.tensor(action_x_prob_list,dtype = torch.float),\
+                                        torch.tensor(action_y_prob_list,dtype = torch.float),\
+                                        torch.tensor(done_list,dtype = torch.float)
+                                        
+        return s,action_x,action_y,r,next_s,action_x_prob,action_y_prob, done_mask
+    
+    def train(self):
+        state, action_x,action_y, reward, next_state,action_x_prob,\
+        action_y_prob, done_mask = self.make_batch()
+        for i in range(K_epoch):
+            td_error = reward + gamma * self.get_value(next_state) * done_mask
+            delta = td_error - self.get_value(state)
+            delta = delta.detach().numpy()
+            advantage_list = []
+            advantage = 0.0
+            for delta_t in delta[::-1]:
+                advantage = gamma * lmbda * advantage + delta_t[0]
+                advantage_list.append([advantage])
+            advantage_list.reverse()
+            advantage = torch.tensor(advantage_list,dtype = torch.float)
+
+            
+            
+            now_action,now_action_prob = self.get_action(state,[action_x[0], action_y[0]])
+
+            now_action_x_type_prob = now_action_prob[0]
+            now_action_y_type_prob = now_action_prob[1]
+            
+            now_action_prob = now_action_x_type_prob + now_action_y_type_prob
+            action_prob = action_x_prob + action_x_prob
+            
+            ratio = torch.exp((now_action_prob) - (action_prob[0]))
+            surr1 = ratio * advantage
+            surr2 = torch.clamp(ratio , 1-eps_clip, 1 + eps_clip) * advantage
+            loss = - torch.min(surr1,surr2) + F.smooth_l1_loss(self.get_value(state),td_error.detach())
+            
+            self.optimizer.zero_grad()
+            loss.mean().backward()
+            self.optimizer.step()
